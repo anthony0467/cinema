8939dd81408397daf0ad41e4081a0f333667e76a
anthony <anthonyspiteri@hotmail.fr>
2023-03-20 10:11:59 +0100

    ajout form realisateur

--- controller/RealController.php
+++ controller/RealController.php
@@ -41,7 +41,37 @@ class RealController {
         //On relie par un "require" la vue qui nous intéresse (située dans le dossier "view")
         require "view/detailReal.php";
     }
-}
 
+    public function addReal(){
+
+       
+        if(isset($_POST["submit"])){
+
+            // on se connecte et On exécute la requête de notre choix 
+            $pdo = Connect::seConnecter();
+            $nomPersonne = filter_input(INPUT_POST, "nom", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $prenomPersonne = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
+            $sexe = filter_input(INPUT_POST, 'sexe',FILTER_SANITIZE_SPECIAL_CHARS);
+            $date_naissance = filter_input(INPUT_POST, "date", FILTER_SANITIZE_SPECIAL_CHARS);
+            $photo = filter_input(INPUT_POST, "photo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $requeteaddPersonne = $pdo->prepare("
+            INSERT INTO personne (nom, prenom, sexe, date_naissance, photo) 
+            VALUES (:nom, :prenom, :sexe, :date, :photo);");
+            $requeteaddPersonne->execute(["nom" => $nomPersonne, "prenom" => $prenomPersonne, "sexe" => $sexe, "date" => $date_naissance, "photo" => $photo]);
+
+             // méthode pour récupéré l'id de l'element inséré 
+             $last_id = $pdo->lastInsertId();
+
+                $requeteAddReal = $pdo->prepare("
+                INSERT INTO realisateur (id_personne) 
+                VALUES (:personneId)");
+                $requeteAddReal->execute(["personneId"=>$last_id]);
+            
+
+        
+            header("Location: index.php?action=listReals");
+      }
+   }
+}
 
 ?>
--- view/listReals.php
+++ view/listReals.php
@@ -2,9 +2,20 @@
 
 <p class="uk-label uk-label-warning txt-center">Total : <?= $requete->rowCount() ?> réalisateurs</p>
 
-<form action="index.php?action=addPersonne&type=<?= $_GET["type"] ?>" class="form-style" method="POST">
+<form action="index.php?action=addReal" class="form-style" method="POST">
     <input type="text" name="nom" id="nom" placeholder="Ajouter un nom">
     <input type="text" name="prenom" id="prenom" placeholder="Ajouter un prénom">
+    <fieldset>
+        <legend disabled selected>Sexe :</legend>
+        <label for="Homme">
+            <input type="radio" name="sexe" value="Homme"> Homme
+        </label>
+        <label for="Femme">
+            <input type="radio" name="sexe" value="Femme"> Femme
+        </label>
+    </fieldset>
+    <input type="date" name="date" id="date" placeholder="01/01/2000">
+    <input type="url" name="photo" id="photo" placeholder="image(url)">
     <input type="submit" name="submit" class="btn" value="Ajouter">
 </form>
 
