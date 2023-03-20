8b18315bbbd52297002166a436b56400874d0839
anthony <anthonyspiteri@hotmail.fr>
2023-03-20 11:14:53 +0100

    ajout form acteur

--- controller/ActeurController.php
+++ controller/ActeurController.php
@@ -45,6 +45,38 @@ class ActeurController {
     }
 
 
+    public function addActeur(){
+
+       
+        if(isset($_POST["submit"])){
+
+            // on se connecte et On exécute la requête de notre choix 
+            $pdo = Connect::seConnecter();
+            $nomPersonne = filter_input(INPUT_POST, "nom", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $prenomPersonne = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
+            $sexe = filter_input(INPUT_POST, 'sexe',FILTER_SANITIZE_SPECIAL_CHARS);
+            $date_naissance = filter_input(INPUT_POST, "date", FILTER_SANITIZE_SPECIAL_CHARS);
+            $photo = filter_input(INPUT_POST, "photo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $requeteaddPersonne = $pdo->prepare("
+            INSERT INTO personne (nom, prenom, sexe, date_naissance, photo) 
+            VALUES (:nom, :prenom, :sexe, :date, :photo);");
+            $requeteaddPersonne->execute(["nom" => $nomPersonne, "prenom" => $prenomPersonne, "sexe" => $sexe, "date" => $date_naissance, "photo" => $photo]);
+
+             // méthode pour récupéré l'id de l'element inséré 
+             $last_id = $pdo->lastInsertId();
+
+                $requeteAddReal = $pdo->prepare("
+                INSERT INTO acteur (id_personne) 
+                VALUES (:personneId)");
+                $requeteAddReal->execute(["personneId"=>$last_id]);
+            
+
+        
+            header("Location: index.php?action=listActeurs");
+      }
+   }
+
+
 }
 
 
--- view/listActeurs.php
+++ view/listActeurs.php
@@ -2,6 +2,23 @@
 
 <p class="uk-label uk-label-warning txt-center">Total : <?= $requete->rowCount() ?> acteurs</p>
 
+<form action="index.php?action=addActeur" class="form-style" method="POST">
+    <input type="text" name="nom" id="nom" placeholder="Ajouter un nom">
+    <input type="text" name="prenom" id="prenom" placeholder="Ajouter un prénom">
+    <fieldset>
+        <legend disabled selected>Sexe :</legend>
+        <label for="Homme">
+            <input type="radio" name="sexe" value="Homme"> Homme
+        </label>
+        <label for="Femme">
+            <input type="radio" name="sexe" value="Femme"> Femme
+        </label>
+    </fieldset>
+    <input type="date" name="date" id="date" placeholder="01/01/2000">
+    <input type="url" name="photo" id="photo" placeholder="image(url)">
+    <input type="submit" name="submit" class="btn" value="Ajouter">
+</form>
+
 <table class="uk-table uk-table-striped">
     <thead>
         <tr>
